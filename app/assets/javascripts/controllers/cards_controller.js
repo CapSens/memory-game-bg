cd0ef8d29e67772ad977bb0ad72c579300d605f7
Christopher Tavares <christavares33@gmail.com>
2025-05-16 17:33:47 +0200

    zaelknazjoe

--- app/assets/javascripts/controllers/cards_controller.js
+++ app/assets/javascripts/controllers/cards_controller.js
@@ -1,9 +1,18 @@
 class CardsController extends Stimulus.Controller {
-  connect() {
-    for (let step = 0; step <= 13; step++) {
+  static targets = ["cardsCountInput", "cardsContainer"]
+
+  createGame(event) {
+    event.preventDefault()
+    this.cardsContainerTarget.innerHTML = ""
+    const cardsCount = this.cardsCountInputTarget.value
+    console.log(this.cardsCountInputTarget.value);
+
+    const shuffled = [...Array(2)].flatMap(() => Array.from({ length: cardsCount }, (_, i) => i + 1))
+      .sort(() => Math.random() - 0.5);
+    shuffled.forEach((step) => {
       addCard(step)
-      addCard(step)
-    }
+    });
+
   }
 
   flip(event) {
